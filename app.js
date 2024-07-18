c180845498c1465f6bb57650d6085cddbb5860f1
shallo3017 <shalmong30@gmail.com>
2024-07-18 17:57:02 +0530

    Adding files

---
+++ app.js
@@ -0,0 +1,16 @@
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+
+for(let select of dropdowns){
+    for(currCode in countryList){
+        let newOption = document.createElement("option");
+        newOption.innerText = currCode;
+        newOption.value = currCode;
+        if(select.name === "from" && currCode ==="USD"){
+            newOption.selected = "selected";
+        }else if(select.name === "to" && currCode ==="INR"){
+            newOption.selected = "selected";
+        }
+        select.append(newOption);
+    }
+}
